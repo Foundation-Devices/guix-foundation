aa1ad8444578b6b9f97d4af3d96e70ef4c449bea
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2025-01-09 14:14:06 +0100

    SFT-4674: Update roam to 143.0.0.beta001.

--- foundation/packages/messaging.scm
+++ foundation/packages/messaging.scm
@@ -13,7 +13,7 @@
 (define-public roam
   (package
     (name "roam")
-    (version "139.0.0-beta001")
+    (version "143.0.0-beta001")
     (source (origin
               (method url-fetch)
               ;; Taken from the Arch Linux's PKGBUILD for Roam.
@@ -25,7 +25,7 @@
                                   version "-roam_" version "_amd64.deb"))
               (sha256
                (base32
-                "0q5ic315fnxkdxc3nvaszfv17lwim1xfgf7vs3x98jjhmmsrbfr4"))))
+                "0p0sp2857md7cx96jps86hbpp54xzixgxf5n5ilzzn15kqp74370"))))
     (build-system chromium-binary-build-system)
     (arguments
      (list #:validate-runpath? #f
