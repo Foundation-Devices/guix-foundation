86aac82082a7b279d98c2f909ad55bf1003c51ad
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2025-05-26 12:51:13 +0200

    Update roam to 162.0.0-beta001.
    * foundation/packages/messaging.scm (roam): Update to
    162.0.0-beta001.

--- foundation/packages/messaging.scm
+++ foundation/packages/messaging.scm
@@ -14,7 +14,7 @@
 (define-public roam
   (package
     (name "roam")
-    (version "160.0.0-beta001")
+    (version "162.0.0-beta001")
     (source (origin
               (method url-fetch)
               ;; Taken from the Arch Linux's PKGBUILD for Roam.
@@ -26,7 +26,7 @@
                                   version "-roam_" version "_amd64.deb"))
               (sha256
                (base32
-                "0b2x1mjdkp6nfmmxrq97z649316hqdxaxnvxigr8g9a6qdpids52"))))
+                "0v9a2fbgjf1n4n80jysp8l5mx4kkpbl3dnvb44hqnqkknxj91lda"))))
     (build-system chromium-binary-build-system)
     (arguments
      (list #:validate-runpath? #f
